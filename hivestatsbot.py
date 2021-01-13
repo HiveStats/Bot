be8a6495b4689702a96375a340fde96fe1b95248
Xd_pro <62808970+Xd-pro@users.noreply.github.com>
2021-01-13 16:37:38 +0000

    fix botstats again...

--- hivestatsbot.py
+++ hivestatsbot.py
@@ -67,7 +67,7 @@ async def survivalgames(ctx, username):
 
 @client.command()
 async def botstats(ctx):
-    ctx.send("Bot is in" + str(len(client.guilds)) + "servers!")
+    await ctx.send("Bot is in" + str(len(client.guilds)) + "servers!")
 
 @survivalgames.error
 async def skywars_error(ctx, error):
