eefd2bae8b97834fde965754e9143c054d5b5b57
Xd_pro <62808970+Xd-pro@users.noreply.github.com>
2021-01-15 13:42:12 +0000

    reformat code by using newlines better

--- hivestatsbot.py
+++ hivestatsbot.py
@@ -76,7 +76,17 @@ async def skywarsleaderboard(ctx):
     print("Python:")
     print(data)
     print("Data type: " + str(type(data)))
-    await ctx.send("**Skywars Leaderboard**\n```ahk\n#1 " + data[0]['username'] + "(" + str(data[0]['victories']) + " Wins)" + "\n#2 " + data[1]['username'] + "(" + str(data[1]['victories']) + " Wins)" + "\n#3 " + data[2]['username'] + "(" + str(data[2]['victories']) + " Wins)" + "\n#4 " + data[3]['username'] + "(" + str(data[3]['victories']) + " Wins)" + "\n#5 " + data[4]['username'] + "(" + str(data[4]['victories']) + " Wins)" + "\n#6 " + data[5]['username'] + "(" + str(data[5]['victories']) + " Wins)" + "\n#7 " + data[6]['username'] + "(" + str(data[6]['victories']) + " Wins)" + "\n#8 " + data[7]['username'] + "(" + str(data[7]['victories']) + " Wins)" + "\n#9 " + data[8]['username'] + "(" + str(data[8]['victories']) + " Wins)" + "\n#10 " + data[9]['username'] + "(" + str(data[9]['victories']) + " Wins)```")
+    await ctx.send("**Skywars Leaderboard**\n```ahk\n"
+                   "#1 " + data[0]['username'] + "(" + str(data[0]['victories']) + " Wins)"
+                   + "\n#2 " + data[1]['username'] + "(" + str(data[1]['victories']) + " Wins)"
+                   + "\n#3 " + data[2]['username'] + "(" + str(data[2]['victories']) + " Wins)"
+                   + "\n#4 " + data[3]['username'] + "(" + str(data[3]['victories']) + " Wins)"
+                   + "\n#5 " + data[4]['username'] + "(" + str(data[4]['victories']) + " Wins)"
+                   + "\n#6 " + data[5]['username'] + "(" + str(data[5]['victories']) + " Wins)"
+                   + "\n#7 " + data[6]['username'] + "(" + str(data[6]['victories']) + " Wins)"
+                   + "\n#8 " + data[7]['username'] + "(" + str(data[7]['victories']) + " Wins)"
+                   + "\n#9 " + data[8]['username'] + "(" + str(data[8]['victories']) + " Wins)"
+                   + "\n#10 " + data[9]['username'] + "(" + str(data[9]['victories']) + " Wins)```")
 
 @client.command()
 async def botstats(ctx):
@@ -99,10 +109,22 @@ async def treasurewars_error(ctx, error):
 
 @client.command()
 async def invite(ctx):
-    await ctx.send("**Hive stats bot (unofficial)**\nAdd to your server: https://discord.com/api/oauth2/authorize?client_id=797497827118284860&permissions=8&scope=bot")
+    await ctx.send("**Hive stats bot (unofficial)**\nAdd to your server: "
+                   "https://discord.com/api/oauth2/authorize?client_id=797497827118284860&permissions=8&scope=bot")
 
 @client.command()
 async def help(ctx):
-    await ctx.send("**Hive stats bot (unofficial)**```html\nPrefix: hive!\n\nskywars <Username> - view a player's skywars stats\ntreasurewars <Username> - view a player's treasure wars stats\nsurvivalgames <Username> - view a player's survival games stats\nskywarsleaderboard - check the top 10 skywars players (Beta)\nhelp - view this\ninvite - add the bot to your server\nbotstats - check bot's stats```\n**YOU NEED TO USE \"username with spaces\" IF A USERNAME HAS SPACES IN IT!**\nNeed help? https://discord.gg/sWxV7WajhW\n\nCheck out Anata, our partner bot! <https://bit.ly/3nL4SYG>")
+    await ctx.send("**Hive stats bot (unofficial)**"
+                   "```html\nPrefix: hive!\n\n"
+                   "skywars <Username> - view a player's skywars stats\n"
+                   "treasurewars <Username> - view a player's treasure wars stats\n"
+                   "survivalgames <Username> - view a player's survival games stats\n"
+                   "skywarsleaderboard - check the top 10 skywars players (Beta)\n"
+                   "help - view this\n"
+                   "invite - add the bot to your server\n"
+                   "botstats - check bot's stats```\n"
+                   "**YOU NEED TO USE \"username with spaces\" IF A USERNAME HAS SPACES IN IT!**\n"
+                   "Need help? https://discord.gg/sWxV7WajhW\n"
+                   "\nCheck out Anata, our partner bot! <https://bit.ly/3nL4SYG>")
 
 client.run(os.environ['token'])
